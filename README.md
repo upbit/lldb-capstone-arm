98312ca92ccbf25713bda475d17f57e80509d002
upbit <rmusique@gmail.com>
2014-12-02 22:33:00 +0800

    add --full options for addr info

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 lldb-capstone-arm
 =================
 
+A lldb script for disassemble ARM(Thumb)/ARM64 code by [Capstone Engine](https://github.com/aquynh/capstone)
+
 ## Setup
 
 1. Unzip and move `*.py` to **~/.lldb**
@@ -12,6 +14,40 @@ or add `command script import ~/.lldb/dis_capstone.py` to **~/.lldbinit** (creat
 
 ## Example
 
-Disassemble arm/thumb with *dis_capstone*:
+Thumb code disassemble compare:
 
 ![Screenshot](https://raw.github.com/upbit/lldb-capstone-arm/master/screenshot.png)
+
+*discs* with -f and -h:
+
+~~~
+(lldb) discs -f
+  SBThread: tid = 0x357e9, frame #0: 0x31c366ba libobjc.A.dylib`objc_retain + 10
+      Address: libobjc.A.dylib[0x2f2286ba] (libobjc.A.dylib.__TEXT.__text + 116410)
+      Summary: libobjc.A.dylib`objc_retain + 10
+
+-> 0x31c366ba:  09 7C            ldrb     r1, [r1, #0x10]
+   0x31c366bc:  11 F0 02 0F      tst.w    r1, #2
+   0x31c366c0:  18 BF            it       ne
+   0x31c366c2:  00 F0 8F B9      b.w      #0x31c369e4
+   0x31c366c6:  47 F6 0A 21      movw     r1, #0x7a0a
+   0x31c366ca:  C0 F2 CF 21      movt     r1, #0x2cf
+   0x31c366ce:  79 44            add      r1, pc
+   0x31c366d0:  09 68            ldr      r1, [r1]
+   0x31c366d2:  09 68            ldr      r1, [r1]
+   0x31c366d4:  F2 F7 44 BC      b.w      #0x31c28f60
+   0x31c366d8:  F0 B5            push     {r4, r5, r6, r7, lr}
+
+(lldb) discs -h
+Usage: discs (-f) (-s <addr>) (-l <len>) (-A <arm|arm64>) (-M <arm|thumb>)
+
+Options:
+   -h, --help            show this help message and exit
+   -s START_ADDR, --start-addr=START_ADDR
+                         start address (default: pc)
+   -l LENGTH, --length=LENGTH
+                         decode bytes length (default: 32)
+   -A ARCH, --arch=ARCH  arch type: arm,arm64 (default: arm)
+   -M MODE, --mode=MODE  mode type: arm,thumb (auto select by cpsr[b:5])
+   -f, --full            show full outputs
+~~~
--- dis_capstone.py
+++ dis_capstone.py
@@ -27,7 +27,7 @@ def __lldb_init_module (debugger, dict):
 def _is_cpsr_thumb(frame):
 	""" Check Thumb flag from CPSR """
 	try:
-		regs = frame.GetRegisters()[0]
+		regs = frame.GetRegisters()[0]	# general purpose registers
 		cpsr = [reg for reg in regs if reg.GetName()=='cpsr'][0]
 		thumb_bit = int(cpsr.GetValue(), 16) & 0x20
 		return thumb_bit >> 5
@@ -38,14 +38,84 @@ def create_command_arguments(command):
 	return shlex.split(command)
 
 def create_options_parser():
-	usage = "Usage: %prog (-s <addr>) (-l <len>) (-A <arm|arm64>) (-M <arm|thumb>)"
+	usage = "Usage: %prog (-f) (-s <addr>) (-l <len>) (-A <arm|arm64>) (-M <arm|thumb>)"
 	parser = optparse.OptionParser(prog='discs', usage=usage)
 	parser.add_option('-s', '--start-addr', dest='start_addr', help='start address (default: pc)', default=None)
 	parser.add_option('-l', '--length', dest='length', help='decode bytes length (default: 32)', default=32)
 	parser.add_option('-A', '--arch', dest='arch', help='arch type: arm,arm64 (default: arm)', default="arm")
 	parser.add_option('-M', '--mode', dest='mode', help='mode type: arm,thumb (auto select by cpsr[b:5])', default=None)
+	parser.add_option('-f', '--full', action="store_true", dest="full", help='show full outputs', default=False)
 	return parser
 
+# from http://www.opensource.apple.com/source/lldb/lldb-69/test/lldbutil.py
+def get_module_names(thread):
+	""" Returns a sequence of module names from the stack frames of this thread. """
+	def GetModuleName(i):
+		return thread.GetFrameAtIndex(i).GetModule().GetFileSpec().GetFilename()
+	return map(GetModuleName, range(thread.GetNumFrames()))
+
+def get_function_names(thread):
+	""" Returns a sequence of function names from the stack frames of this thread. """
+	def GetFuncName(i):
+		return thread.GetFrameAtIndex(i).GetFunctionName()
+	return map(GetFuncName, range(thread.GetNumFrames()))
+
+def get_symbol_names(thread):
+	""" Returns a sequence of symbols for this thread. """
+	def GetSymbol(i):
+		return thread.GetFrameAtIndex(i).GetSymbol().GetName()
+	return map(GetSymbol, range(thread.GetNumFrames()))
+
+def get_filenames(thread):
+	""" Returns a sequence of file names from the stack frames of this thread. """
+	def GetFilename(i):
+		return thread.GetFrameAtIndex(i).GetLineEntry().GetFileSpec().GetFilename()
+	return map(GetFilename, range(thread.GetNumFrames()))
+
+def get_line_numbers(thread):
+	""" Returns a sequence of line numbers from the stack frames of this thread. """
+	def GetLineNumber(i):
+		return thread.GetFrameAtIndex(i).GetLineEntry().GetLine()
+	return map(GetLineNumber, range(thread.GetNumFrames()))
+
+def get_pc_addresses(thread):
+	""" Returns a sequence of pc addresses for this thread. """
+	def GetPCAddress(i):
+		return thread.GetFrameAtIndex(i).GetPCAddress()
+	return map(GetPCAddress, range(thread.GetNumFrames()))
+
+def back_stacktrace(target, thread):
+	mods = get_module_names(thread)
+	functions = get_function_names(thread)
+	symbols = get_symbol_names(thread)
+	files = get_filenames(thread)
+	lines = get_line_numbers(thread)
+	addrs = get_pc_addresses(thread)
+
+	for i in range(thread.GetNumFrames()):
+		frame = thread.GetFrameAtIndex(i)
+		function = frame.GetFunction()
+
+		load_addr = addrs[i].GetLoadAddress(target)
+		if not function:
+			file_addr = addrs[i].GetFileAddress()
+			start_addr = frame.GetSymbol().GetStartAddress().GetFileAddress()
+			symbol_offset = file_addr - start_addr
+			if (symbol_offset < 0):
+				symbol_offset = 0
+			print "  frame #{num}: {addr:#016x} {mod}`{symbol} + {offset}".format(
+				num=i, addr=load_addr, mod=mods[i], symbol=symbols[i], offset=symbol_offset)
+		else:
+			print "  frame #{num}: {addr:#016x} {mod}`{func} at {file}:{line} {args}".format(
+				num=i, addr=load_addr, mod=mods[i],
+				func='%s [inlined]' % funcs[i] if frame.IsInlined() else funcs[i],
+				file=files[i], line=lines[i], args=get_args_as_string(frame, showFuncName=False))
+
+
+def image_lookup_addr(addr):
+	res = lldb.SBCommandReturnObject()
+	lldb.debugger.GetCommandInterpreter().HandleCommand("image lookup --address 0x%x" % addr, res)
+	return res.GetOutput()
 
 def real_disassemble(debugger, start_addr, disasm_length, disasm_arch, disasm_mode):
 	""" Disassemble code with target arch/mode """
@@ -121,6 +191,10 @@ def dis_capstone(debugger, command, result, dict):
 	if (options.arch == "arm64"):
 		disasm_mode = CS_MODE_ARM
 
+	# show frame and addr info
+	if (options.full):
+		print "  %s, %s" % (thread, frame)
+		print image_lookup_addr(start_addr)
 
 	##
 	real_disassemble(debugger, start_addr, disasm_length, disasm_arch, disasm_mode)
